53577405b423f7d559c6588a9bc481bbbea92ea2
BROOKS R LEISURE <brooksleisure90@gmail.com>
2026-05-24 03:34:48 -0400

    Implement service worker with caching functionality

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
+const CACHE_NAME = 'beart-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c => c.addAll(urlsToCache))
+      .catch(err => console.log('SW cache failed:', err))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+  );
+});
