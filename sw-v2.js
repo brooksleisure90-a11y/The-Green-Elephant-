f25e898553f7fbce6210310068219dae8973d9e5
BROOKS R LEISURE <brooksleisure90@gmail.com>
2026-05-24 08:07:08 -0400

    Create sw-v2.js

---
+++ sw-v2.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'beart-v2';
+const urlsToCache = ['./', './index.html', './manifest.json', './icon-512.png', './icon-192.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c => c.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
