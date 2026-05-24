dc494cee0f02346bf1f440ce4b0fb78ac9b062d3
BROOKS R LEISURE <brooksleisure90@gmail.com>
2026-05-24 03:33:43 -0400

    Remove service worker caching logic

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE_NAME = 'beart-v1';
-const urlsToCache = ['beart.html', 'manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
